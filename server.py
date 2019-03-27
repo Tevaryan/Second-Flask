9fbd326494281048d9c5255ae6f2498bbd34b35a
Tevaryan Thiagarajan <tevaryan1993@gmail.com>
2019-03-27 09:23:38 +0800

    added non-functioning contact us form

--- server.py
+++ server.py
@@ -1,17 +1,15 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
-    return render_template('index.html')
-
+    last_name = request.args.get('last_name')
+    first_name = request.args.get('first_name')
+    return render_template('index.html', last_name=last_name, first_name =first_name)
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
-
 
 if __name__ == '__main__':
     app.run()
